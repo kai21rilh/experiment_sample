3d80c4e5ed892f2fa5dc0f0d3c9c20745ce843f0
kai21rilh <83002521+kai21rilh@users.noreply.github.com>
2023-07-04 02:01:45 +1000

    renamed and updated

--- experiment/main.js
+++ experiment/main.js
@@ -2,20 +2,20 @@
 const jsPsych = initJsPsych({
   display_element: "display_stage",
   experiment_width: 1100,
-  default_iti: 250,
+  default_iti: 400,
   on_finish: function () {
     var datajs = jsPsych.data.get().json();
-    Qualtrics.SurveyEngine.setEmbeddedData("idnum", idnum);
-    Qualtrics.SurveyEngine.setEmbeddedData("assignment_c1", assignment_c1);
-    Qualtrics.SurveyEngine.setEmbeddedData("assignment_c2", assignment_c2);
+    Qualtrics.SurveyEngine.setEmbeddedData("pw", password);
+    Qualtrics.SurveyEngine.setEmbeddedData("c1", assignment_c1);
+    Qualtrics.SurveyEngine.setEmbeddedData("c2", assignment_c2);
     Qualtrics.SurveyEngine.setEmbeddedData("datajs", datajs);
     jQuery("#display_stage").remove();
     jQuery("#display_stage_background").remove();
   },
 });
 
-// id (random digits)
-const idnum = jsPsych.randomization.randomInt(10000000, 99999999);
+// password (random digits)
+const password = jsPsych.randomization.randomInt(10000, 99999);
 
 // condition assignment
 // define levels
@@ -41,23 +41,30 @@ const next_text = "先に進む";
 const img_hourglass =
   "https://kai21rilh.github.io/experiment_sample/experiment/img/hourglass_animated.gif";
 
-// test-run
-const start = {
-  type: jsPsychHtmlButtonResponse,
-  stimulus:
-    "<p>これから、テストランを開始します。</p>" +
-    "<p>下のボタンを押して、先に進んでください。</p>",
-  choices: [next_text],
+// loading (e.g., images)
+const preload = {
+  type: jsPsychPreload,
+  auto_preload: true,
 };
 
+// test-run (start full-screen mode)
+const start = {
+  type: jsPsychFullscreen,
+  fullscreen_mode: true,
+  message:
+    "これから、テストランを開始します。<br><br>準備ができたら、下のボタンを押してください。",
+  button_label: next_text,
+};
+
+// sample questions
 const q1 = {
   type: jsPsychSurveyText,
   preamble: "テスト用刺激文です。",
   questions: [
     {
-      prompt: "30と入力してください。",
+      prompt: "何か数字を入力してください。",
       placeholder: "ここに入力",
-      name: "question_01",
+      name: "q1",
       columns: 30,
       required: true,
     },
@@ -70,9 +77,9 @@ const q2 = {
   preamble: "テスト用刺激文その２です。",
   questions: [
     {
-      prompt: "選択肢２を選んでください。",
+      prompt: "いずれかの選択肢を選択してください。",
       labels: ["選択肢１", "選択肢２", "選択肢３"],
-      name: "question_02",
+      name: "q2",
       required: true,
     },
   ],
@@ -80,49 +87,47 @@ const q2 = {
   button_label: next_text,
 };
 
+// sample instruction
 const inst = {
   type: jsPsychHtmlButtonResponse,
   stimulus:
-    "<p>教示文のテストです。</p>" +
-    "<p>下のボタンを押して、先に進んでください。</p>",
+    "教示文のテストです。<br><br>下のボタンを押して、先に進んでください。",
   choices: [next_text],
 };
 
-// const buttonpress = {
-//   type: jsPsychHtmlKeyboardResponse,
-//   stimulus: "何かキーを押してください。次の画面に進みます。",
-//   choices: "ALL_KEYS",
-// }; // does not work ... a bug?
-
+// sample image stimulus
 var wp = jsPsych.randomization.randomInt(5000, 10000);
 const img_stim = {
   type: jsPsychImageKeyboardResponse,
   stimulus: img_hourglass,
-  stimulus_width: 280,
-  stimulus_height: 210,
+  stimulus_width: 240,
+  stimulus_height: 180,
   maintain_aspect_ratio: false,
   render_on_canvas: false,
   prompt: "<p>5秒～10秒で先に進みます。</p >",
   choices: "NO_KEYS",
   trial_duration: wp,
-  post_trial_gap: 2000,
+  post_trial_gap: 3000,
 };
 
+// end the test-run
 const end = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus:
-    "<p>以上でテストランは終了です。</p>" +
-    "<p>最後にID番号をお伝えします。<br>この番号は、あなた個人に割り当てられるものです。</p>" +
-    "<p>あなたのIDは <b>" +
-    idnum +
-    " </b>です。</p>" +
-    '<p style="color:red">30秒後にテストランが終了します。忘れずにID番号をメモしてください。',
+    "以上でテストランは終了です。<br>最後にパスワードをお伝えします。<br><br>本研究のパスワードは <b>" +
+    password +
+    ' </b>です。<br><br><font color="#c00000">20秒後にテストランが終了します。忘れずにメモしてください。</font>',
   choices: "NO_KEYS",
-  trial_duration: 30000,
+  trial_duration: 20000,
+};
+
+// exit full-screen mode
+const exit_fs = {
+  type: jsPsychFullscreen,
+  fullscreen_mode: false,
 };
 
 // timeline
-const sample_program = {
-  // timeline: [start, q1, q2, inst, buttonpress, img_stim, end],
-  timeline: [start, q1, q2, inst, img_stim, end],
+const questionnaire = {
+  timeline: [preload, start, q1, q2, inst, img_stim, end, exit_fs],
 };
